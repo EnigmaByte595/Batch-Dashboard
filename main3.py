8dcbb428c30e9f8b0149986512f10c592da87d9f
Sneha pal <sabitaapal1976@gmail.com>
2025-09-26 15:06:01 +0530

    Your descriptive message here

--- main3.py
+++ main3.py
@@ -5,6 +5,29 @@ from datetime import datetime
 
 st.set_page_config(page_title="IIEST Civil-Met Hub", layout="wide")
 
+st.markdown(
+    """
+    <style>
+    .book-link {
+        display: inline-block;
+        padding: 8px 14px;
+        margin: 5px 0;
+        background-color: #262730;
+        color: #FFD700 !important;
+        text-decoration: none;
+        border-radius: 8px;
+        font-weight: 600;
+        box-shadow: 2px 2px 6px rgba(0,0,0,0.3);
+    }
+    .book-link:hover {
+        background-color: #333333;
+        color: white !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("IIEST Civil-Met Notes & Resources")
 
 # --- Navigation Tabs ---
@@ -14,74 +37,149 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7,tab8 , tab9= st.tabs(
 
 # --- Tab 1: Syllabus ---
 with tab1:
+
+    def book_card(name, link):
+        st.markdown(
+            f"""
+            <div style='
+                background-color:#1E1E1E;
+                padding:10px;
+                margin:6px 0;
+                border-radius:10px;
+                box-shadow:2px 2px 6px rgba(0,0,0,0.4);
+            '>
+                <a href="{link}" target="_blank" style='color:#2980B9; text-decoration:none; font-size:16px; font-weight:600;'>
+                    {name}
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
     st.header("Syllabus")
-    st.markdown("[Physics Syllabus](https://drive.google.com/file/d/1u3QJSGBeNrry3zmk75aJJNzvTEMjwB3k/view?usp=drive_link)")
-    st.markdown("[Maths Syllabus](https://drive.google.com/file/d/1R20GWLIeyLrvGyVubwCJzygXPhxaIQnB/view?usp=drive_link)")
+    book_card("Physics Syllabus","https://drive.google.com/file/d/1u3QJSGBeNrry3zmk75aJJNzvTEMjwB3k/view?usp=drive_link)")
+    book_card("Maths Syllabus","https://drive.google.com/file/d/1R20GWLIeyLrvGyVubwCJzygXPhxaIQnB/view?usp=drive_link")
     #st.markdown("[AI_ML Syllabus]()")
-    st.markdown("[materials](https://drive.google.com/file/d/1-MyY4tZKGMwT1iclaeekGZ3D39X3BfUn/view?usp=drive_link)")
-    st.markdown("[energy_env_climate](https://drive.google.com/file/d/1J866HBgAwWaakmc1kfuLMypHfQHnNbLG/view?usp=sharing)")
+    book_card("materials","https://drive.google.com/file/d/1-MyY4tZKGMwT1iclaeekGZ3D39X3BfUn/view?usp=drive_link")
+    book_card("energy_env_climate","https://drive.google.com/file/d/1J866HBgAwWaakmc1kfuLMypHfQHnNbLG/view?usp=sharing")
     #st.markdown("[Engineering_drawing]()")
+
 
 # --- Tab 2: PYQs ---
 with tab2:
+
+    def book_card(name, link):
+        st.markdown(
+            f"""
+            <div style='
+                background-color:#1E1E1E;
+                padding:10px;
+                margin:6px 0;
+                border-radius:10px;
+                box-shadow:2px 2px 6px rgba(0,0,0,0.4);
+            '>
+                <a href="{link}" target="_blank" style='color:#F39C12; text-decoration:none; font-size:16px; font-weight:600;'>
+                    {name}
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
     st.header("pyq")
     pyq = {
         "Maths": {
-            "2017_MA-1101":"https://drive.google.com/file/d/11lOd5H1rRadfqFL27fvBgLkns4y5pL9g/view?usp=sharing",
-            "2016_MA_1101":"https://drive.google.com/file/d/11lOd5H1rRadfqFL27fvBgLkns4y5pL9g/view?usp=sharing",
-            "2015_MA_1101":"https://drive.google.com/file/d/11lOd5H1rRadfqFL27fvBgLkns4y5pL9g/view?usp=sharing",
-            "2020-21_MA_1101":"https://drive.google.com/file/d/1IL1x3cecxPsGAXYsFtD9XgGoqDheLwke/view?usp=drive_link",
-            "2022_MA_mid_sem":"https://drive.google.com/file/d/1MbRvk1Wg1fnbH-rTMjEY4prgIby4CeNY/view?usp=drive_link",
-            "2023_MA_1101_mid":"https://drive.google.com/file/d/14WptP5_jNHpd7bRs1bEyzVUevDwnlAOA/view?usp=drive_link",
-
-
+            "2017_MA-1101": "https://drive.google.com/file/d/11lOd5H1rRadfqFL27fvBgLkns4y5pL9g/view?usp=sharing",
+            "2016_MA_1101": "https://drive.google.com/file/d/11lOd5H1rRadfqFL27fvBgLkns4y5pL9g/view?usp=sharing",
+            "2015_MA_1101": "https://drive.google.com/file/d/11lOd5H1rRadfqFL27fvBgLkns4y5pL9g/view?usp=sharing",
+            "2020-21_MA_1101": "https://drive.google.com/file/d/1IL1x3cecxPsGAXYsFtD9XgGoqDheLwke/view?usp=drive_link",
+            "2022_MA_mid_sem": "https://drive.google.com/file/d/1MbRvk1Wg1fnbH-rTMjEY4prgIby4CeNY/view?usp=drive_link",
+            "2023_MA_1101_mid": "https://drive.google.com/file/d/14WptP5_jNHpd7bRs1bEyzVUevDwnlAOA/view?usp=drive_link",
         },
-        
-        "Physics":{
-            "2021_PH_1101":"https://drive.google.com/file/d/17OhlHtvOn8w7eHbiOHgZjuScbHT8pbKC/view?usp=sharing",
-            "2017_PH_1101":"https://drive.google.com/file/d/1RSl_8Nk4ecLemjBcfcOTo1bda_8-pCYu/view?usp=drive_link",
-            "MECHA_2021_1101":"https://drive.google.com/file/d/1V_F-4Il5zt0gvGS8mXxdg4-Jc79iSgVg/view?usp=drive_link",
-            "PH_2020-21_1101":"https://drive.google.com/file/d/1RRIQKscrrSj84uL_r0OPfUPpFK6XzHMN/view?usp=drive_link",
-            "MECHA_2022_MID_1101":"https://drive.google.com/file/d/1MbRvk1Wg1fnbH-rTMjEY4prgIby4CeNY/view?usp=drive_link",
-            "mecha_2023_end_1101":"https://drive.google.com/file/d/1vjgNjWsBfNX1k6C-RK1a_bdKqxa0M_-e/view?usp=drive_link",
-            "mecha_2021_mid_soln":"https://drive.google.com/file/d/1O6JMxsTWdAapOZ8QUj0VwnhGojPZOPZh/view?usp=drive_link",
-
-
-
-
+        "Physics": {
+            "2021_PH_1101": "https://drive.google.com/file/d/17OhlHtvOn8w7eHbiOHgZjuScbHT8pbKC/view?usp=sharing",
+            "2017_PH_1101": "https://drive.google.com/file/d/1RSl_8Nk4ecLemjBcfcOTo1bda_8-pCYu/view?usp=drive_link",
+            "MECHA_2021_1101": "https://drive.google.com/file/d/1V_F-4Il5zt0gvGS8mXxdg4-Jc79iSgVg/view?usp=drive_link",
+            "PH_2020-21_1101": "https://drive.google.com/file/d/1RRIQKscrrSj84uL_r0OPfUPpFK6XzHMN/view?usp=drive_link",
+            "MECHA_2022_MID_1101": "https://drive.google.com/file/d/1MbRvk1Wg1fnbH-rTMjEY4prgIby4CeNY/view?usp=drive_link",
+            "mecha_2023_end_1101": "https://drive.google.com/file/d/1vjgNjWsBfNX1k6C-RK1a_bdKqxa0M_-e/view?usp=drive_link",
+            "mecha_2021_mid_soln": "https://drive.google.com/file/d/1O6JMxsTWdAapOZ8QUj0VwnhGojPZOPZh/view?usp=drive_link",
         }
     }
 
     for subject, papers in pyq.items():
-        with st.expander(subject):
+        with st.expander(f"**{subject}**"):
             for paper, link in papers.items():
-                st.markdown(f"- [{paper}]({link})")
+                book_card(paper, link)
+    
+
     
 
 # --- Tab 3: Books ---
 with tab3:
+
+    def book_card(name, link):
+        st.markdown(
+            f"""
+            <div style='
+                background-color:#1E1E1E;
+                padding:10px;
+                margin:6px 0;
+                border-radius:10px;
+                box-shadow:2px 2px 6px rgba(0,0,0,0.4);
+            '>
+                <a href="{link}" target="_blank" style='color:#8E44AD; text-decoration:none; font-size:16px; font-weight:600;'>
+                    {name}
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
+    
     st.header("Books Referred by Professors")
-    st.markdown("[wave and oscillation - NK Bajaj](https://drive.google.com/file/d/115xtoNhhJhrpgCqjwTdNZnk0Lq421sSW/view?usp=sharing)")
-    st.markdown("[engineering_mechanics_dynamics -- Meriam & Kraige](https://drive.google.com/file/d/1RmyPBGEKn-qwXb2lkUlxfNQhryj-LHhs/view?usp=drive_link)")
-    st.markdown("[optics-Ajoy Ghatak](https://drive.google.com/file/d/1zmDjO9GWLBDqJXfIV4ZSfxeHrkTej4ab/view?usp=drive_link)")
-    st.markdown("[Introduction to Electrodynamics-griffiths](https://drive.google.com/file/d/1zvnwOvivXpYq52NUmtFvpfrE6hsv6iIQ/view?usp=drive_link)")
-    st.markdown("[optics-Eugene Hecht](https://drive.google.com/file/d/1R15VPR7EDGBP0GZ_-GPkqRppDfUZzOe0/view?usp=drive_link)")
-    st.markdown("[Quantum Physics-Robert Eisberg and Robert Resnik](https://drive.google.com/file/d/1m9Twnc5sn_Tv3Yy1OFlRqVblCqCJ_WdD/view?usp=drive_link)")
-    st.markdown("[concepts of modern physics-Arthur Beiser](https://drive.google.com/file/d/1KPDAMoUSEMWoGRJm3zMDQMlV6vjnQqMT/view?usp=drive_link)")
-    st.markdown("[Free and forced vibrations-DP_roychoudhury](https://drive.google.com/file/d/1YSmz30MzgjhdWlaLXWIRCdsAPsJFIVMv/view?usp=drive_link)")
-    st.markdown("[Advanced engineering mathematics-kreyszig](https://drive.google.com/file/d/19cuVy4mMbmoQbAYnoKpVcGPYxaSCK8np/view?usp=drive_link)")
-    st.markdown("[Engineering mathematics-SS SASTRY](https://drive.google.com/file/d/10vCcObvmvxOOky-XycGMX2VEwYXsg_a0/view?usp=drive_link)")
-    st.markdown("[Integral calculus & diff. eqn](https://drive.google.com/file/d/1ReQu37i1qbTYO4QLMHLOYtRR9rJcgz1z/view?usp=drive_link)")
-    st.markdown("[Advanced calculus-DV widder](https://drive.google.com/file/d/1BfajFPEg5d0WE40Qn7g12J5c7WhZij4w/view?usp=drive_link)")
-    st.markdown("[engineering mechanics-mariam & kraige](https://drive.google.com/file/d/1RmyPBGEKn-qwXb2lkUlxfNQhryj-LHhs/view?usp=drive_link)")
-    st.markdown("[engineering mechanics-mariam & kraige-solution](https://drive.google.com/file/d/1mKJZmNWfFUkfYR-qL_9EIWX5mBOyNXsP/view?usp=drive_link)")
-    st.markdown("[engineering mechanics dynamics-meriam & kraige](https://drive.google.com/file/d/1IX9mE9JHqT_oL7ch2MTnMurNAhLGUWIk/view?usp=drive_link)")
-    st.markdown("[engineering mechanics statics-meriam & kraige](https://drive.google.com/file/d/1aAdahyyFnhp7xP8KOByaIYJioTNG2CDY/view?usp=drive_link)")
-    st.markdown("[manufacturing engineering and technology-S vijay sekhar](https://drive.google.com/file/d/1A7-f9UI__vZ-UVVFtfdSc_oFP02E3Wiq/view?usp=drive_link)")
-    st.markdown("[gps(1st sem)](https://drive.google.com/file/d/1jBvb9R74WDrWwgmaOJJSwHsJSfExmEP3/view?usp=drive_link)")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("📘 Physics")
+        book_card("Wave and Oscillation - NK Bajaj", "https://drive.google.com/file/d/115xtoNhhJhrpgCqjwTdNZnk0Lq421sSW/view?usp=sharing")
+        book_card("engineering_mechanics_dynamics -- Meriam & Kraige","https://drive.google.com/file/d/1RmyPBGEKn-qwXb2lkUlxfNQhryj-LHhs/view?usp=drive_link")
+        book_card("optics-Ajoy Ghatak","https://drive.google.com/file/d/1zmDjO9GWLBDqJXfIV4ZSfxeHrkTej4ab/view?usp=drive_link")
+        book_card("Introduction to Electrodynamics-griffiths","https://drive.google.com/file/d/1zvnwOvivXpYq52NUmtFvpfrE6hsv6iIQ/view?usp=drive_link")
+        book_card("optics-Eugene Hecht","https://drive.google.com/file/d/1R15VPR7EDGBP0GZ_-GPkqRppDfUZzOe0/view?usp=drive_link")
+        book_card("Quantum Physics-Robert Eisberg and Robert Resnik","https://drive.google.com/file/d/1m9Twnc5sn_Tv3Yy1OFlRqVblCqCJ_WdD/view?usp=drive_link")
+        book_card("concepts of modern physics-Arthur Beiser","https://drive.google.com/file/d/1KPDAMoUSEMWoGRJm3zMDQMlV6vjnQqMT/view?usp=drive_link")
+        book_card("Free and forced vibrations-DP_roychoudhury","https://drive.google.com/file/d/1YSmz30MzgjhdWlaLXWIRCdsAPsJFIVMv/view?usp=drive_link")
+        book_card("engineering mechanics-mariam & kraige","https://drive.google.com/file/d/1RmyPBGEKn-qwXb2lkUlxfNQhryj-LHhs/view?usp=drive_link")
+        book_card("engineering mechanics-mariam & kraige-solution","https://drive.google.com/file/d/1mKJZmNWfFUkfYR-qL_9EIWX5mBOyNXsP/view?usp=drive_link")
+        book_card("engineering mechanics dynamics-meriam & kraige","https://drive.google.com/file/d/1IX9mE9JHqT_oL7ch2MTnMurNAhLGUWIk/view?usp=drive_link")
+        book_card("engineering mechanics statics-meriam & kraige","https://drive.google.com/file/d/1aAdahyyFnhp7xP8KOByaIYJioTNG2CDY/view?usp=drive_link")
+    with col2:
+        st.subheader("📗 Mathematics")    
+        book_card("Advanced engineering mathematics-kreyszig","https://drive.google.com/file/d/19cuVy4mMbmoQbAYnoKpVcGPYxaSCK8np/view?usp=drive_link")
+        book_card("Engineering mathematics-SS SASTRY","https://drive.google.com/file/d/10vCcObvmvxOOky-XycGMX2VEwYXsg_a0/view?usp=drive_link")
+        book_card("Integral calculus & diff. eqn","https://drive.google.com/file/d/1ReQu37i1qbTYO4QLMHLOYtRR9rJcgz1z/view?usp=drive_link")
+        book_card("Advanced calculus-DV widder","https://drive.google.com/file/d/1BfajFPEg5d0WE40Qn7g12J5c7WhZij4w/view?usp=drive_link")
+        book_card("manufacturing engineering and technology-S vijay sekhar","https://drive.google.com/file/d/1A7-f9UI__vZ-UVVFtfdSc_oFP02E3Wiq/view?usp=drive_link")
+        book_card("gps(1st sem","https://drive.google.com/file/d/1jBvb9R74WDrWwgmaOJJSwHsJSfExmEP3/view?usp=drive_link")
 
 # --- Tab 4: Classnotes ---
 with tab4:
+    def book_card(name, link):
+        st.markdown(
+            f"""
+            <div style='
+                background-color:#1E1E1E;
+                padding:10px;
+                margin:6px 0;
+                border-radius:10px;
+                box-shadow:2px 2px 6px rgba(0,0,0,0.4);
+            '>
+                <a href="{link}" target="_blank" style='color:#E67E22; text-decoration:none; font-size:16px; font-weight:600;'>
+                    {name}
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
     st.header("Classnotes")
     classnotes = {
         "Maths": {
@@ -148,40 +246,95 @@ with tab4:
     for subject, chapters in classnotes.items():
         with st.expander(subject):
             for chapter, link in chapters.items():
-                st.markdown(f"- [{chapter}]({link})")
+                book_card(chapter, link)
 
 # --- Tab 5: Lab Notes ---
 with tab5:
+
     st.header("Lab Notes")
     st.write("will update soon")
 
 # --- Tab 6: Drawing Plates ---
 with tab6:
+    def book_card(name, link):
+        st.markdown(
+            f"""
+            <div style='
+                background-color:#1E1E1E;
+                padding:10px;
+                margin:6px 0;
+                border-radius:10px;
+                box-shadow:2px 2px 6px rgba(0,0,0,0.4);
+            '>
+                <a href="{link}" target="_blank" style='color:#E91E63; text-decoration:none; font-size:16px; font-weight:600;'>
+                    {name}
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
     st.header("Engineering Drawing")
-    st.markdown("[Plate 1](https://drive.google.com/file/d/12EnfY4K7jU44tMil1zmY12neoGumgZtC/view?usp=drive_link)")
-    st.markdown("[Plate 2](https://drive.google.com/file/d/1pYd_ecgkcGMP-6dodt4cCSuLhTyT6XB5/view?usp=drive_link)")
-    st.markdown("[Plate 3](https://drive.google.com/file/d/1zVcv9G50FbpmcL8tLJRioEt1SKJqLgEl/view?usp=drive_link)")
-    st.markdown("[Plate 4](https://drive.google.com/file/d/1h42sX1H2OckqK3e6DW8t750RejpfcMfT/view?usp=drive_link)")
-    st.markdown("[plate 5](https://drive.google.com/file/d/1FpCtrrH18297cae3Q8aycGIhDAQQjg53/view?usp=drive_link)")
-    st.markdown("[plate 6](https://drive.google.com/file/d/1azU_s8oFA-sDpbEnIdDh3pO2BzUPZRk6/view?usp=drive_link)")
-    st.markdown("[plate 7](https://drive.google.com/file/d/1PU9ZmBvwUCF0GNPnQQSGiLkwwFsYlJSk/view?usp=drive_link)")
-    st.markdown("[ED pyq](https://drive.google.com/file/d/1YeiEFJs4TysuL0MRsyRf5LCZKsOhVE5i/view?usp=drive_link)")
-    st.markdown("[Assignment](https://drive.google.com/file/d/1dekTEMA1j0Xumf-wUu_d0wlwGUP3mo2H/view?usp=drive_link)")
-    st.markdown("[ED viva](https://drive.google.com/file/d/1oBawP4nIfz3KsLrUfLMUXVBfE8gMEiaC/view?usp=drive_link)")
-    st.markdown("[Ed Book-N.D.BHATT](https://drive.google.com/file/d/1qGiz2NjwAwwAVjithmeP-PC-6vu1SNmJ/view?usp=drive_link)")
-    st.markdown("[all plates](https://drive.google.com/file/d/1DJyxjuUqUY50DOPW7OIYGN919fkBe7A-/view?usp=drive_link)")
+    book_card("Plate 1","https://drive.google.com/file/d/12EnfY4K7jU44tMil1zmY12neoGumgZtC/view?usp=drive_link")
+    book_card("Plate 2","https://drive.google.com/file/d/1pYd_ecgkcGMP-6dodt4cCSuLhTyT6XB5/view?usp=drive_link")
+    book_card("Plate 3","https://drive.google.com/file/d/1zVcv9G50FbpmcL8tLJRioEt1SKJqLgEl/view?usp=drive_link")
+    book_card("Plate 4","https://drive.google.com/file/d/1h42sX1H2OckqK3e6DW8t750RejpfcMfT/view?usp=drive_link")
+    book_card("plate 5","https://drive.google.com/file/d/1FpCtrrH18297cae3Q8aycGIhDAQQjg53/view?usp=drive_link")
+    book_card("plate 6","https://drive.google.com/file/d/1azU_s8oFA-sDpbEnIdDh3pO2BzUPZRk6/view?usp=drive_link")
+    book_card("plate 7","https://drive.google.com/file/d/1PU9ZmBvwUCF0GNPnQQSGiLkwwFsYlJSk/view?usp=drive_link")
+    book_card("ED pyq","https://drive.google.com/file/d/1YeiEFJs4TysuL0MRsyRf5LCZKsOhVE5i/view?usp=drive_link")
+    book_card("Assignment","https://drive.google.com/file/d/1dekTEMA1j0Xumf-wUu_d0wlwGUP3mo2H/view?usp=drive_link")
+    book_card("ED viva","https://drive.google.com/file/d/1oBawP4nIfz3KsLrUfLMUXVBfE8gMEiaC/view?usp=drive_link")
+    book_card("Ed Book-N.D.BHATT","https://drive.google.com/file/d/1qGiz2NjwAwwAVjithmeP-PC-6vu1SNmJ/view?usp=drive_link")
+    book_card("all plates","https://drive.google.com/file/d/1DJyxjuUqUY50DOPW7OIYGN919fkBe7A-/view?usp=drive_link")
 # --- Tab 7: Assignments ---
 with tab7:
+    def book_card(name, link):
+        st.markdown(
+            f"""
+            <div style='
+                background-color:#1E1E1E;
+                padding:10px;
+                margin:6px 0;
+                border-radius:10px;
+                box-shadow:2px 2px 6px rgba(0,0,0,0.4);
+            '>
+                <a href="{link}" target="_blank" style='color:#27AE60; text-decoration:none; font-size:16px; font-weight:600;'>
+                    {name}
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
     st.header("Assignments")
-    st.markdown("[physics problem set-1](https://drive.google.com/file/d/1m7TFOjLHyQPkicUaRNkJzcb0S7ZCJg1j/view?usp=sharing)")
-    st.markdown("[math problemset-1](https://drive.google.com/file/d/1engBCWYFKwaFTALKEdYrSfS2akSJkTfu/view?usp=sharing)")
-    st.markdown("[Infinite_series_soln](https://drive.google.com/file/d/1eyKunaxhiZJNNt-8639MVZDUy8a0oLH9/view?usp=drive_link)")
+    book_card("physics problem set-1","https://drive.google.com/file/d/1m7TFOjLHyQPkicUaRNkJzcb0S7ZCJg1j/view?usp=sharing")
+    book_card("math problemset-1","https://drive.google.com/file/d/1engBCWYFKwaFTALKEdYrSfS2akSJkTfu/view?usp=sharing")
+    book_card("Infinite_series_soln","https://drive.google.com/file/d/1eyKunaxhiZJNNt-8639MVZDUy8a0oLH9/view?usp=drive_link")
 # --- Tab8:Routine ---
 with tab8:
+    def book_card(name, link):
+        st.markdown(
+            f"""
+            <div style='
+                background-color:#1E1E1E;
+                padding:10px;
+                margin:6px 0;
+                border-radius:10px;
+                box-shadow:2px 2px 6px rgba(0,0,0,0.4);
+            '>
+                <a href="{link}" target="_blank" style='color:#6C757D; text-decoration:none; font-size:16px; font-weight:600;'>
+                    {name}
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
     st.header("CIVIL-MET ROUTINE")
-    st.markdown("[Routine](https://drive.google.com/file/d/11lOd5H1rRadfqFL27fvBgLkns4y5pL9g/view?usp=sharing)")
+    book_card("Routine","https://drive.google.com/file/d/11lOd5H1rRadfqFL27fvBgLkns4y5pL9g/view?usp=sharing")
 
 with tab9:
+
     FILE = "comments.csv"
 
     # Load old comments safely (handle empty file)
